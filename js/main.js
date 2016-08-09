f828468e216ec80993373bd3c92ee183c16e313e
Andre Souza <andresouza4@gmail.com>
2016-08-08 17:07:37 -0700

    Portfolio Section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    lazyLoad: true,
+    loop: true,
+    nav: true,
+    navText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
+  });
+});
