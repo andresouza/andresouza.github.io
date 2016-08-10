1c1972892d57ca2b7bc7aa7b57b4ca8d33c6e686
Andre Souza <andresouza4@gmail.com>
2016-08-09 22:56:24 -0700

    Added Projects to Portfolio

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     items: 1,
     lazyLoad: true,
     loop: true,
+    autoHeight:true,
     nav: true,
     navText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
   });
